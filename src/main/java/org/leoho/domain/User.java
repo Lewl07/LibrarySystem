dd8c3d9f7491ef4b7dd6777373c6385b99a18da0
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 13:48:01 -0400

    Complete User fields and constructor code

--- src/main/java/org/leoho/domain/User.java
+++ src/main/java/org/leoho/domain/User.java
@@ -1,4 +1,25 @@
 package org.leoho.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@EqualsAndHashCode
 public abstract class User {
+    private static int nextId = 1;
+
+    protected String id;
+    protected String name;
+    protected List<Item> borrowedItems;
+
+    public User(String name) {
+        this.id = String.format("%04d", nextId++);
+        this.name = name;
+        this.borrowedItems = new ArrayList<>();
+    }
 }
