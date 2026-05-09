47b9887b9b815593dadd1845b5568a260e7bbbba
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 13:51:23 -0400

    Complete fields and constructor code for Student and Teacher

--- src/main/java/org/leoho/domain/Student.java
+++ src/main/java/org/leoho/domain/Student.java
@@ -1,4 +1,8 @@
 package org.leoho.domain;
 
 public class Student extends User {
+
+    public Student(String name) {
+        super(name);
+    }
 }
--- src/main/java/org/leoho/domain/Teacher.java
+++ src/main/java/org/leoho/domain/Teacher.java
@@ -1,4 +1,8 @@
 package org.leoho.domain;
 
 public class Teacher extends User {
+
+    public Teacher(String name) {
+        super(name);
+    }
 }
