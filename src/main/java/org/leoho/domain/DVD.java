66c7dad17acd614365f9ff0ce7c7818504550f0a
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 14:12:05 -0400

    Complete DVD fields and constructor

--- src/main/java/org/leoho/domain/DVD.java
+++ src/main/java/org/leoho/domain/DVD.java
@@ -1,4 +1,12 @@
 package org.leoho.domain;
 
 public class DVD extends Item {
+    private String director;
+    private int duration;
+
+    public DVD(String title, Status status, String director, int duration) {
+        super(title, status);
+        this.director = director;
+        this.duration = duration;
+    }
 }
