977680e796d4b5937e6ab7b71dc3b611efd5d342
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 14:14:18 -0400

    Complete Book fields and constructor

--- src/main/java/org/leoho/domain/Book.java
+++ src/main/java/org/leoho/domain/Book.java
@@ -1,4 +1,14 @@
 package org.leoho.domain;
 
 public class Book extends Item {
+    private String isbn;
+    private String author;
+    private String genre;
+
+    public Book(String title, Status status, String isbn, String author, String genre) {
+        super(title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
 }
