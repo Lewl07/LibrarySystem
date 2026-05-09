f6cde96807c1e4fd16d049ec7d097a0ab3a898d4
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 14:42:08 -0400

    Add java doc for isValidISBN()

--- src/main/java/org/leoho/util/Validation.java
+++ src/main/java/org/leoho/util/Validation.java
@@ -1,6 +1,12 @@
 package org.leoho.util;
 
 public class Validation {
+
+    /**
+     * Checks whether the ISBN is valid or not
+     * @param isbn the isbn
+     * @return whether the ISBN is valid or not
+     */
     public static boolean isValidISBN(String isbn) {
         return isbn.matches("\\d{13}");
     }
