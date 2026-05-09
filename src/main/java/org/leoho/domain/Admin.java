e613e2740a3ad6265761484ee84eb0afba6f9ecc
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 17:28:01 -0400

    Implement methods from User to subclasses

--- src/main/java/org/leoho/domain/Admin.java
+++ src/main/java/org/leoho/domain/Admin.java
@@ -2,10 +2,32 @@ package org.leoho.domain;
 
 import org.leoho.interfaces.Reportable;
 
+import java.util.List;
+
 public class Admin extends User implements Reportable {
 
     public Admin(String name) {
         super(name);
+    }
+
+    @Override
+    public boolean borrowItem(Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean returnItem(Item item) {
+        return false;
+    }
+
+    @Override
+    public List<Item> searchTitleRecursive(String keyword) {
+        return List.of();
+    }
+
+    @Override
+    public List<Item> searchAuthorStream(String keyword) {
+        return List.of();
     }
 
     @Override
--- src/main/java/org/leoho/domain/Teacher.java
+++ src/main/java/org/leoho/domain/Teacher.java
@@ -1,8 +1,30 @@
 package org.leoho.domain;
 
+import java.util.List;
+
 public class Teacher extends User {
 
     public Teacher(String name) {
         super(name);
+    }
+
+    @Override
+    public boolean borrowItem(Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean returnItem(Item item) {
+        return false;
+    }
+
+    @Override
+    public List<Item> searchTitleRecursive(String keyword) {
+        return List.of();
+    }
+
+    @Override
+    public List<Item> searchAuthorStream(String keyword) {
+        return List.of();
     }
 }
