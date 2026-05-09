e9b4f700ca04e4ce4fc573bab6b70980e4abdd5d
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 14:05:04 -0400

    Complete Item fields and constructor

--- src/main/java/org/leoho/domain/Item.java
+++ src/main/java/org/leoho/domain/Item.java
@@ -1,4 +1,23 @@
 package org.leoho.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@Getter
+@ToString
 public abstract class Item {
+    private static int nextId = 1;
+
+    protected String id;
+    protected String title;
+    @Setter protected Status status;
+
+    public Item(String title, Status status) {
+        this.title = title;
+        this.id = String.format("%04d", nextId++);
+        this.status = status;
+    }
 }
