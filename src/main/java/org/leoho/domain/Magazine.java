88e9fdd6c79c5215c3899cdbb0a73092537a1296
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-09 14:09:23 -0400

    Complete Magazine fields and constructor

--- src/main/java/org/leoho/domain/Magazine.java
+++ src/main/java/org/leoho/domain/Magazine.java
@@ -1,4 +1,12 @@
 package org.leoho.domain;
 
 public class Magazine extends Item {
+    private int issueNumber;
+    private String publisher;
+
+    public Magazine(String title, Status status, int issueNumber, String publisher) {
+        super(title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
