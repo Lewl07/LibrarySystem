453cb0f8b55d97967be686b7e98eaf23151afb10
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-12 00:07:05 -0400

    Complete borrow and return methods for Teacher

--- src/main/java/org/leoho/domain/Teacher.java
+++ src/main/java/org/leoho/domain/Teacher.java
@@ -2,6 +2,7 @@ package org.leoho.domain;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.leoho.util.Constants;
 
 import java.util.HashSet;
 import java.util.List;
@@ -15,14 +16,41 @@ public class Teacher extends User {
         super(name, library);
     }
 
+    /**
+     * A user can borrow items, with a borrowing limit of 5 books (BOOKS ONLY) for students, and
+     * a limit of 10 items for teachers.
+     * @param item the item
+     * @return whether the borrowing was a success or not
+     */
     @Override
     public boolean borrowItem(Item item) {
-        return false;
+        if (borrowedItems.size() >= Constants.MAX_ITEMS_TEACHER) {
+            throw new IllegalArgumentException("Teachers can only borrow up to 10 items.");
+        }
+
+        if (item.getStatus() != Status.IN_STORE) {
+            throw new IllegalArgumentException("The item is not available.");
+        }
+
+        item.setStatus(Status.BORROWED);
+
+        return borrowedItems.add(item);
     }
 
+    /**
+     * A user can return items that was previously borrowed
+     * @param item the item
+     * @return whether the item was successfully returned
+     */
     @Override
     public boolean returnItem(Item item) {
-        return false;
+        if (!borrowedItems.contains(item)) {
+            throw new IllegalArgumentException("You do not have this book in possession.");
+        }
+
+        item.setStatus(Status.IN_STORE);
+
+        return borrowedItems.remove(item);
     }
 
     @Override
