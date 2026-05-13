d6e9d3eff27c7835b07665d1e008ff336ff2b89e
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-13 11:59:00 -0400

    Add more Constants CSV paths

--- src/main/java/org/leoho/util/Constants.java
+++ src/main/java/org/leoho/util/Constants.java
@@ -3,6 +3,14 @@ package org.leoho.util;
 public class Constants {
     public static final int MAX_BOOKS_STUDENT = 5;
     public static final int MAX_ITEMS_TEACHER = 10;
-    public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
+
+    public static final String BOOKS_CSV_PATH = "src/main/resources/books.csv";
+    public static final String DVDS_CSV_PATH = "src/main/resources/dvds.csv";
+    public static final String MAGAZINES_CSV_PATH = "src/main/resources/magazines.csv";
+
+    public static final String STUDENTS_CSV_PATH = "src/main/resources/students.csv";
+    public static final String TEACHERS_CSV_PATH = "src/main/resources/teachers.csv";
+
     public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
+    public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
 }
