457b4a782e0fc25812dfb495a2c907c36b031c39
Leo <6375071@edu.vaniercollege.qc.ca>
2026-05-13 15:17:57 -0400

    Run a demo for User guide

--- src/main/java/org/leoho/Main.java
+++ src/main/java/org/leoho/Main.java
@@ -1,7 +1,22 @@
 package org.leoho;
 
+import org.leoho.domain.*;
+
 public class Main {
     public static void main(String[] args) {
+        Library library = new Library();
+        Admin admin = new Admin("leoAdministrator01", library);
 
+        admin.initUsers();
+        admin.initItems();
+
+//        library.getItems().add(new Book
+//                ("deletedBook", org.leoho.domain.Status.IN_STORE,
+//                "1234567890123", "Leo", "inexistant")
+//        );
+
+        admin.generateReport();
+
+        System.out.println(library.getUsers().getFirst().searchItemRecursive("fake"));
     }
 }
